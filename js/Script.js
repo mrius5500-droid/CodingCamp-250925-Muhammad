f3cb499c8f7295515f5c60e8ce2b14c2013276f2
mrius5500-droid <mrius5500@gmail.com>
2025-09-27 09:57:05 +0700

    Add files via upload

---
+++ js/Script.js
@@ -0,0 +1,8 @@
+alert('Selamat datang..');
+var lagi = true;
+while (lagi) {
+  var nama = prompt('Masukkan nama anda: ');
+  alert('Halo ' + nama);
+    lagi = confirm('Coba lagi?');
+}
+alert('Terima kasih sudah berkunjung..');
